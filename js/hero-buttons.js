9102391c381f1f3e9a446bfde957f7e7e3a87009
Tom <tom@dnomaid.co.uk>
2019-02-09 23:58:20 +0000

    add Windows modal

--- js/hero-buttons.js
+++ js/hero-buttons.js
@@ -6,6 +6,8 @@ if (plat) {
     os = 'mac';
   } else if (plat.includes('linux')) {
     os = 'linux';
+  } else if (plat.includes('win')) {
+    os = 'win';
   }
 
   if (os !== undefined) {
@@ -24,4 +26,11 @@ function showBoth() {
 
 function showBeta() {
   document.getElementById('buttBeta').style.display = 'inline-block';
+}
+
+if(os == 'win') {
+  doModal(`
+      <h2><i class="fab fa-windows"></i> Using Windows?</h2>
+      <p>MagicCap is a capture suite for macOS and Linux only. On Windows, we recommend the similar FOSS project, <a href="https://getsharex.com/">ShareX</a>.</p>
+    `);
 }
